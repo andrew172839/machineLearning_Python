0f14cb2ae6828600aad7d9cfbfa799d01c9b9307
andrew789 <yushao.cheng@gmail.com>
2017-10-31 13:55:27 +0800

    .

--- py/plot_adaboost_hastie_10_2.py
+++ py/plot_adaboost_hastie_10_2.py
@@ -20,11 +20,6 @@ whereas real SAMME.R uses the predicted class probabilities.
 """
 print(__doc__)
 
-# Author: Peter Prettenhofer <peter.prettenhofer@gmail.com>,
-#         Noel Dawe <noel.dawe@gmail.com>
-#
-# License: BSD 3 clause
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -44,44 +39,32 @@ learning_rate = 1.
 #X_test, y_test = X[2000:], y[2000:]
 #X_train, y_train = X[:2000], y[:2000]
 
-
 a = pd.read_csv('sample20170117_labeled_0207.csv')
 X = a.values[0: 100, 0: 110]
 y = a.values[0: 100, 110]
 y = np.array([1 if i == 1. else -1 for i in y])
 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(
-      X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size = 0.2, random_state = 42)
 
-dt_stump = DecisionTreeClassifier(max_depth=1, min_samples_leaf=1)
+dt_stump = DecisionTreeClassifier(max_depth = 1, min_samples_leaf = 1)
 dt_stump.fit(X_train, y_train)
 dt_stump_err = 1.0 - dt_stump.score(X_test, y_test)
 
-dt = DecisionTreeClassifier(max_depth=9, min_samples_leaf=1)
+dt = DecisionTreeClassifier(max_depth = 9, min_samples_leaf = 1)
 dt.fit(X_train, y_train)
 dt_err = 1.0 - dt.score(X_test, y_test)
 
-ada_discrete = AdaBoostClassifier(
-    base_estimator=dt_stump,
-    learning_rate=learning_rate,
-    n_estimators=n_estimators,
-    algorithm="SAMME")
+ada_discrete = AdaBoostClassifier(base_estimator = dt_stump, learning_rate = learning_rate, n_estimators = n_estimators, algorithm = "SAMME")
 ada_discrete.fit(X_train, y_train)
 
-ada_real = AdaBoostClassifier(
-    base_estimator=dt_stump,
-    learning_rate=learning_rate,
-    n_estimators=n_estimators,
-    algorithm="SAMME.R")
+ada_real = AdaBoostClassifier(base_estimator = dt_stump, learning_rate = learning_rate, n_estimators = n_estimators, algorithm = "SAMME.R")
 ada_real.fit(X_train, y_train)
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
-ax.plot([1, n_estimators], [dt_stump_err] * 2, 'k-',
-        label='Decision Stump Error')
-ax.plot([1, n_estimators], [dt_err] * 2, 'k--',
-        label='Decision Tree Error')
+ax.plot([1, n_estimators], [dt_stump_err] * 2, 'k-', label = 'Decision Stump Error')
+ax.plot([1, n_estimators], [dt_err] * 2, 'k--', label = 'Decision Tree Error')
 
 ada_discrete_err = np.zeros((n_estimators,))
 for i, y_pred in enumerate(ada_discrete.staged_predict(X_test)):
@@ -99,24 +82,16 @@ ada_real_err_train = np.zeros((n_estimators,))
 for i, y_pred in enumerate(ada_real.staged_predict(X_train)):
     ada_real_err_train[i] = zero_one_loss(y_pred, y_train)
 
-ax.plot(np.arange(n_estimators) + 1, ada_discrete_err,
-        label='Discrete AdaBoost Test Error',
-        color='red')
-ax.plot(np.arange(n_estimators) + 1, ada_discrete_err_train,
-        label='Discrete AdaBoost Train Error',
-        color='blue')
-ax.plot(np.arange(n_estimators) + 1, ada_real_err,
-        label='Real AdaBoost Test Error',
-        color='orange')
-ax.plot(np.arange(n_estimators) + 1, ada_real_err_train,
-        label='Real AdaBoost Train Error',
-        color='green')
+ax.plot(np.arange(n_estimators) + 1, ada_discrete_err, label = 'Discrete AdaBoost Test Error', color = 'red')
+ax.plot(np.arange(n_estimators) + 1, ada_discrete_err_train, label = 'Discrete AdaBoost Train Error', color = 'blue')
+ax.plot(np.arange(n_estimators) + 1, ada_real_err, label = 'Real AdaBoost Test Error', color = 'orange')
+ax.plot(np.arange(n_estimators) + 1, ada_real_err_train, label = 'Real AdaBoost Train Error', color = 'green')
 
 ax.set_ylim((0.0, 0.5))
 ax.set_xlabel('n_estimators')
 ax.set_ylabel('error rate')
 
-leg = ax.legend(loc='upper right', fancybox=True)
+leg = ax.legend(loc = 'upper right', fancybox = True)
 leg.get_frame().set_alpha(0.7)
 
 plt.show()
