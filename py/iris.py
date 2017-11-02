80b7c9a2797ca9560fd531691316962e785ecd7f
andrew789 <yushao.cheng@gmail.com>
2017-11-02 14:30:48 +0800

    .

--- py/iris.py
+++ py/iris.py
@@ -3,36 +3,35 @@ from __future__ import division
 from __future__ import print_function
 from sklearn import cross_validation
 from sklearn import metrics
+from sklearn.datasets import make_classification
 import tensorflow as tf
 import pandas as pd
 import numpy as np
 
+
 def main(unused_argv):
-  # Load dataset.
-  #iris = tf.contrib.learn.datasets.load_dataset('iris')
-  #x_train, x_test, y_train, y_test = cross_validation.train_test_split(
-  #    iris.data, iris.target, test_size=0.2, random_state=42)
+    # iris = tf.contrib.learn.datasets.load_dataset('iris')
+    # x_train, x_test, y_train, y_test = cross_validation.train_test_split(iris.data, iris.target, test_size=0.2,
+    #                                                                      random_state=42)
 
-  a = pd.read_csv('sample20170117_labeled_0207.csv')
-#a = pd.read_csv('sample20170117_labeled_01.csv')
-  training = a.values[:, 0: 110]
-  label = a.values[:, 110]
-  label = np.array([1 if i == 1. else -1 for i in label])
-  x_train, x_test, y_train, y_test = cross_validation.train_test_split(
-      training, label, test_size=0.2, random_state=42)
+    # X, y = make_classification(n_samples=100, n_features=10, n_classes=2)
 
-  # Build 3 layer DNN with 10, 20, 10 units respectively.
-  feature_columns = tf.contrib.learn.infer_real_valued_columns_from_input(
-      x_train)
-  classifier = tf.contrib.learn.DNNClassifier(
-      feature_columns=feature_columns, hidden_units=[10, 20, 10], n_classes=3)
+    # a = pd.read_csv('sample20170117_labeled_0207.csv')
+    # training = a.values[:, 0: 110]
+    # label = a.values[:, 110]
+    # label = np.array([1 if i == 1. else -1 for i in label])
+    # x_train, x_test, y_train, y_test = cross_validation.train_test_split(training, label, test_size=0.2,
+    #                                                                      random_state=42)
 
-  # Fit and predict.
-  classifier.fit(x_train, y_train, steps=200)
-  predictions = list(classifier.predict(x_test, as_iterable=True))
-  score = metrics.accuracy_score(y_test, predictions)
-  print('Accuracy: {0:f}'.format(score))
+    # Build 3 layer DNN with 10, 20, 10 units respectively.
+    feature_columns = tf.contrib.learn.infer_real_valued_columns_from_input(x_train)
+    classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns, hidden_units=[10, 20, 10], n_classes=3)
+
+    classifier.fit(x_train, y_train, steps=200)
+    predictions = list(classifier.predict(x_test, as_iterable=True))
+    score = metrics.accuracy_score(y_test, predictions)
+    print('Accuracy: {0:f}'.format(score))
 
 
 if __name__ == '__main__':
-  tf.app.run()
+    tf.app.run()
--- py/plot_validation_curve.py
+++ py/plot_validation_curve.py
@@ -1,49 +1,45 @@
-"""
-==========================
-Plotting Validation Curves
-==========================
-
-In this plot you can see the training scores and validation scores of an SVM
-for different values of the kernel parameter gamma. For very low values of
-gamma, you can see that both the training score and the validation score are
-low. This is called underfitting. Medium values of gamma will result in high
-values for both scores, i.e. the classifier is performing fairly well. If gamma
-is too high, the classifier will overfit, which means that the training score
-is good but the validation score is poor.
-"""
-print(__doc__)
+# Plotting Validation Curves
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 from sklearn.datasets import load_digits
 from sklearn.svm import SVC
 from sklearn.model_selection import validation_curve
+from sklearn.datasets import make_classification
 
-#digits = load_digits()
-#X, y = digits.data, digits.target
+# digits = load_digits()
+# X, y = digits.data, digits.target
 
-import pandas as pd
-a = pd.read_csv('sample20170117_labeled_0207.csv')
-X = a.values[0: 1000, 0: 110]
-y = a.values[0: 1000, 110]
-y = np.array([1 if i == 1. else -1 for i in y])
+# a = pd.read_csv('sample20170117_labeled_0207.csv')
+# X = a.values[0: 1000, 0: 110]
+# y = a.values[0: 1000, 110]
+# y = np.array([1 if i == 1. else -1 for i in y])
+
+X, y = make_classification(n_samples=1000, n_features=10, n_classes=2, random_state=0)
+
+# X = 1. / (np.arange(1, 11) + np.arange(0, 10)[:, np.newaxis])
+# y = np.ones(10)
 
 param_range = np.logspace(-6, -1, 5)
-train_scores, test_scores = validation_curve(SVC(), X, y, param_name = "gamma", param_range = param_range, cv = 10, scoring = "accuracy", n_jobs = 1)
-train_scores_mean = np.mean(train_scores, axis = 1)
-train_scores_std = np.std(train_scores, axis = 1)
-test_scores_mean = np.mean(test_scores, axis = 1)
-test_scores_std = np.std(test_scores, axis = 1)
+train_scores, test_scores = validation_curve(SVC(), X, y, param_name="gamma", param_range=param_range, cv=10,
+                                             scoring="accuracy", n_jobs=1)
+train_scores_mean = np.mean(train_scores, axis=1)
+train_scores_std = np.std(train_scores, axis=1)
+test_scores_mean = np.mean(test_scores, axis=1)
+test_scores_std = np.std(test_scores, axis=1)
 
 plt.title("Validation Curve with SVM")
 plt.xlabel("$\gamma$")
 plt.ylabel("Score")
 plt.ylim(0.0, 1.1)
 lw = 2
-plt.semilogx(param_range, train_scores_mean, label = "Training score", color = "darkorange", lw = lw)
-plt.fill_between(param_range, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, alpha = 0.2, color = "darkorange", lw = lw)
-plt.semilogx(param_range, test_scores_mean, label = "Cross-validation score", color = "navy", lw = lw)
-plt.fill_between(param_range, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha = 0.2, color = "navy", lw = lw)
-plt.legend(loc = "best")
+plt.semilogx(param_range, train_scores_mean, label="Training score", color="darkorange", lw=lw)
+plt.fill_between(param_range, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, alpha=0.2,
+                 color="darkorange", lw=lw)
+plt.semilogx(param_range, test_scores_mean, label="Cross-validation score", color="navy", lw=lw)
+plt.fill_between(param_range, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.2,
+                 color="navy", lw=lw)
+plt.legend(loc="best")
 plt.show()
