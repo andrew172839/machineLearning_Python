f77cbdc5731fb8a9e797727f6dae3c2d52bb8520
andrew789 <yushao.cheng@gmail.com>
2017-11-02 14:56:26 +0800

    .

--- py/iris_val_based_early_stopping.py
+++ py/iris_val_based_early_stopping.py
@@ -31,7 +31,7 @@ def main(unused_argv):
     x, y = make_classification(n_samples=1000, n_features=100, n_classes=2)
 
     # x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
-    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
     x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, random_state=42)
 
     val_monitor = learn.monitors.ValidationMonitor(x_val, y_val, early_stopping_rounds=200)
