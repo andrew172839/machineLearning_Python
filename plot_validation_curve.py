cf6e17b5881646ffc09b25ae53bd85261b5a834c
andrew172839 <yushao.cheng@gmail.com>
2018-04-14 22:12:09 +0800

    .

--- plot_validation_curve.py
+++ plot_validation_curve.py
@@ -1,4 +1,4 @@
-# Plotting Validation Curves
+# plotting validation curves
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,15 +12,7 @@ from sklearn.datasets import make_classification
 # digits = load_digits()
 # X, y = digits.data, digits.target
 
-# a = pd.read_csv('sample20170117_labeled_0207.csv')
-# X = a.values[0: 1000, 0: 110]
-# y = a.values[0: 1000, 110]
-# y = np.array([1 if i == 1. else -1 for i in y])
-
 X, y = make_classification(n_samples=1000, n_features=10, n_classes=2, random_state=0)
-
-# X = 1. / (np.arange(1, 11) + np.arange(0, 10)[:, np.newaxis])
-# y = np.ones(10)
 
 param_range = np.logspace(-6, -1, 5)
 train_scores, test_scores = validation_curve(SVC(), X, y, param_name="gamma", param_range=param_range, cv=10,
@@ -30,16 +22,16 @@ train_scores_std = np.std(train_scores, axis=1)
 test_scores_mean = np.mean(test_scores, axis=1)
 test_scores_std = np.std(test_scores, axis=1)
 
-plt.title("Validation Curve with SVM")
+plt.title('validation curve with svm')
 plt.xlabel("$\gamma$")
-plt.ylabel("Score")
+plt.ylabel('score')
 plt.ylim(0.0, 1.1)
 lw = 2
-plt.semilogx(param_range, train_scores_mean, label="Training score", color="darkorange", lw=lw)
+plt.semilogx(param_range, train_scores_mean, label='training score', color='darkorange', lw=lw)
 plt.fill_between(param_range, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, alpha=0.2,
-                 color="darkorange", lw=lw)
-plt.semilogx(param_range, test_scores_mean, label="Cross-validation score", color="navy", lw=lw)
+                 color='darkorange', lw=lw)
+plt.semilogx(param_range, test_scores_mean, label='cross-validation score', color='navy', lw=lw)
 plt.fill_between(param_range, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.2,
-                 color="navy", lw=lw)
-plt.legend(loc="best")
+                 color='navy', lw=lw)
+plt.legend(loc='best')
 plt.show()
