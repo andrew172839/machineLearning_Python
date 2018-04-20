ea54bf46f0ce650c1f5564c9f84d816b1269a687
andrew789 <yushao.cheng@gmail.com>
2018-04-20 16:49:49 +0800

    .

--- plot_ridge_path.py
+++ plot_ridge_path.py
@@ -2,18 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model
 
-X = 1. / (np.arange(1, 11) + np.arange(0, 10)[:, np.newaxis])
-y = np.ones(10)
-print(X);
-print(y);
-
 # import pandas as pd
+#
 # a = pd.read_csv('sample20170117_labeled_0207.csv')
 # X = a.values[0: 100, 0: 110]
 # y = a.values[0: 100, 110]
 # y = np.array([1 if i == 1. else -1 for i in y])
 
-n_alphas = 200
+# from sklearn import datasets
+#
+# iris = datasets.load_iris()
+# X = iris.data
+# y = iris.target
+
+# from sklearn.datasets import make_classification
+#
+# X, y = make_classification(n_samples=1000, n_features=100, n_classes=2)
+
+n_alphas = 100
 alphas = np.logspace(-10, -2, n_alphas)
 clf = linear_model.Ridge(fit_intercept=False)
 
@@ -29,6 +35,6 @@ ax.set_xscale('log')
 ax.set_xlim(ax.get_xlim()[::-1])
 plt.xlabel('alpha')
 plt.ylabel('weights')
-plt.title('Ridge coefficients as a function of the regularization')
+plt.title('ridge coefficients')
 plt.axis('tight')
 plt.show()
