17e47d397656e16abb6ef04d4a5f70d123584957
andrew789 <yushao.cheng@gmail.com>
2018-04-24 15:23:57 +0800

    .

--- sklearn/precision_recall.py
+++ sklearn/precision_recall.py
@@ -1,5 +1,3 @@
-# Precision-Recall
-
 import matplotlib.pyplot as plt
 import numpy as np
 from itertools import cycle
@@ -10,38 +8,32 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import label_binarize
 from sklearn.multiclass import OneVsRestClassifier
 
-# iris = datasets.load_iris()
-# X = iris.data
-# y = iris.target
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
 
-import pandas as pd
+# a = pd.read_csv('sample20170117_labeled_0207.csv')
+# X = a.values[0: 100, 0: 110]
+# y = a.values[0: 100, 110]
+# y = np.array([1 if i == 1. else -1 for i in y])
 
-a = pd.read_csv('sample20170117_labeled_0207.csv')
-X = a.values[0: 100, 0: 110]
-y = a.values[0: 100, 110]
-y = np.array([1 if i == 1. else -1 for i in y])
+# X, y = make_classification(n_samples=1000, n_features=100, n_classes=2)
 
-# setup plot details
 colors = cycle(['navy', 'turquoise', 'darkorange', 'cornflowerblue', 'teal'])
 lw = 2
 
-# Binarize the output
 y = label_binarize(y, classes=[0, 1, 2])
 n_classes = y.shape[1]
 
-# Add noisy features
 random_state = np.random.RandomState(0)
 n_samples, n_features = X.shape
 X = np.c_[X, random_state.randn(n_samples, 200 * n_features)]
 
-# Split into training and test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.5, random_state=random_state)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Run classifier
-classifier = OneVsRestClassifier(svm.SVC(kernel='linear', probability=True, random_state=random_state))
+classifier = OneVsRestClassifier(svm.SVC(kernel='linear', probability=True))
 y_score = classifier.fit(X_train, y_train).decision_function(X_test)
 
-# Compute Precision-Recall and plot curve
 precision = dict()
 recall = dict()
 average_precision = dict()
@@ -49,35 +41,30 @@ for i in range(n_classes):
     precision[i], recall[i], _ = precision_recall_curve(y_test[:, i], y_score[:, i])
     average_precision[i] = average_precision_score(y_test[:, i], y_score[:, i])
 
-# Compute micro-average ROC curve and ROC area
 precision["micro"], recall["micro"], _ = precision_recall_curve(y_test.ravel(), y_score.ravel())
 average_precision["micro"] = average_precision_score(y_test, y_score, average="micro")
 
-# Plot Precision-Recall curve
 plt.clf()
-plt.plot(recall[0], precision[0], lw=lw, color='navy',
-         label='Precision-Recall curve')
-plt.xlabel('Recall')
-plt.ylabel('Precision')
+plt.plot(recall[0], precision[0], lw=lw, color='navy', label='precision-recall curve')
+plt.xlabel('recall')
+plt.ylabel('precision')
 plt.ylim([0.0, 1.05])
 plt.xlim([0.0, 1.0])
-plt.title('Precision-Recall example: AUC={0:0.2f}'.format(average_precision[0]))
+plt.title('precision-recall example, auc={0:0.2f}'.format(average_precision[0]))
 plt.legend(loc="lower left")
 plt.show()
 
-# Plot Precision-Recall curve for each class
 plt.clf()
 plt.plot(recall["micro"], precision["micro"], color='gold', lw=lw,
-         label='micro-average Precision-recall curve (area = {0:0.2f})'
-               ''.format(average_precision["micro"]))
+         label='micro-average precision-recall curve (area = {0:0.2f})'''.format(average_precision["micro"]))
 for i, color in zip(range(n_classes), colors):
     plt.plot(recall[i], precision[i], color=color, lw=lw,
-             label='Precision-recall curve of class {0} (area = {1:0.2f})'''.format(i, average_precision[i]))
+             label='precision-recall curve of class {0} (area = {1:0.2f})'''.format(i, average_precision[i]))
 
 plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])
-plt.xlabel('Recall')
-plt.ylabel('Precision')
-plt.title('Extension of Precision-Recall curve to multi-class')
+plt.xlabel('recall')
+plt.ylabel('precision')
+plt.title('extension of precision-recall curve to multi-class')
 plt.legend(loc="lower right")
 plt.show()
