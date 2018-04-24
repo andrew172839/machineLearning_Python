a8755eb32797a24d293802d89ee759d5c0e00e69
andrew789 <yushao.cheng@gmail.com>
2018-04-24 15:11:19 +0800

    .

--- sklearn/l1_logistic_path.py
+++ sklearn/l1_logistic_path.py
@@ -1,29 +1,29 @@
-# path with l1- logistic regression
-
-from sklearn.datasets import make_classification
 from sklearn import linear_model
-from sklearn import datasets
 from sklearn.svm import l1_min_c
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-iris = datasets.load_iris()
-x = iris.data
-y = iris.target
+from sklearn import datasets
+from sklearn.datasets import make_classification
+import pandas as pd
+
+# iris = datasets.load_iris()
+# X = iris.data
+# y = iris.target
 
 # a = pd.read_csv('sample20170117_labeled_0207.csv')
-# x = a.values[0: 100, 0: 110]
+# X = a.values[0: 100, 0: 110]
 # y = a.values[0: 100, 110]
+# y = np.array([1 if i == 1. else -1 for i in y])
 
-# x, y = make_classification(n_samples=100, n_features=10, n_classes=2)
+X, y = make_classification(n_samples=1000, n_features=100, n_classes=2)
 
-cs = l1_min_c(x, y, loss='log') * np.logspace(0, 3)
+cs = l1_min_c(X, y, loss='log') * np.logspace(0, 3)
 clf = linear_model.LogisticRegression(penalty='l1')
 coefs_ = []
 for c in cs:
     clf.set_params(C=c)
-    clf.fit(x, y)
+    clf.fit(X, y)
     coefs_.append(clf.coef_.ravel().copy())
 
 coefs_ = np.array(coefs_)
