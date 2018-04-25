ebc90e93b2f65ae63ec2651d75720929c9539a48
andrew789 <yushao.cheng@gmail.com>
2018-04-25 15:37:23 +0800

    .

--- tensorflow/dnnRegressor.py
+++ tensorflow/dnnRegressor.py
@@ -4,28 +4,37 @@ from __future__ import print_function
 from sklearn import cross_validation
 from sklearn import metrics
 from sklearn import preprocessing
-from sklearn.datasets import make_classification
 import tensorflow as tf
-import pandas as pd
 import numpy as np
+
+from sklearn import datasets
+from sklearn.datasets import make_classification
+import pandas as pd
 
 
 def main(unused_argv):
-    boston = tf.contrib.learn.datasets.load_dataset('boston')
-    x, y = boston.data, boston.target
+    # boston = tf.contrib.learn.datasets.load_dataset('boston')
+    # X, y = boston.data, boston.target
 
-    x_train, x_test, y_train, y_test = cross_validation.train_test_split(x, y, test_size=0.2, random_state=42)
+    a = pd.read_csv('sample20170117_labeled_0207.csv')
+    X = a.values[0: 100, 0: 110]
+    y = a.values[0: 100, 110]
+    y = np.array([1 if i == 1. else -1 for i in y])
+
+    # X, y = make_classification(n_samples=1000, n_features=100, n_classes=2)
+
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 
     scaler = preprocessing.StandardScaler()
-    x_train = scaler.fit_transform(x_train)
+    X_train = scaler.fit_transform(X_train)
 
-    # build 2 layer fully connected dnns with 10, 10 units respectively
-    feature_columns = tf.contrib.learn.infer_real_valued_columns_from_input(x_train)
+    feature_columns = tf.contrib.learn.infer_real_valued_columns_from_input(X_train)
     regressor = tf.contrib.learn.DNNRegressor(feature_columns=feature_columns, hidden_units=[10, 10])
-    regressor.fit(x_train, y_train, steps=5000, batch_size=1)
-    y_predicted = list(regressor.predict(scaler.transform(x_test), as_iterable=True))
+    regressor.fit(X_train, y_train, steps=5000, batch_size=1)
+    y_predicted = list(regressor.predict(scaler.transform(X_test), as_iterable=True))
     score = metrics.mean_squared_error(y_predicted, y_test)
     print('mse, {0: f}'.format(score))
+
 
 if __name__ == '__main__':
     tf.app.run()
