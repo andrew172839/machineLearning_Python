bac1c70f38e06f456fc9812dbf8ecae9ecd8991c
andrew172839 <yushao.cheng@gmail.com>
2018-05-06 11:49:17 +0800

    .

--- sklearn/ridgePath.py
+++ sklearn/ridgePath.py
@@ -1,23 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model
+from sklearn.model_selection import train_test_split
 
-# import pandas as pd
-#
-# a = pd.read_csv('sample20170117_labeled_0207.csv')
-# X = a.values[0: 100, 0: 110]
-# y = a.values[0: 100, 110]
-# y = np.array([1 if i == 1. else -1 for i in y])
+from sklearn import datasets
+from sklearn.datasets import make_classification
+import pandas as pd
 
-# from sklearn import datasets
-#
+a = pd.read_csv('sample20170117_labeled_0207.csv')
+X = a.values[0: 100, 0: 110]
+y = a.values[0: 100, 110]
+y = np.array([1 if i == 1. else -1 for i in y])
+
 # iris = datasets.load_iris()
 # X = iris.data
 # y = iris.target
 
-# from sklearn.datasets import make_classification
-#
 # X, y = make_classification(n_samples=1000, n_features=100, n_classes=2)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 n_alphas = 100
 alphas = np.logspace(-10, -2, n_alphas)
@@ -26,7 +27,7 @@ clf = linear_model.Ridge(fit_intercept=False)
 coefs = []
 for a in alphas:
     clf.set_params(alpha=a)
-    clf.fit(X, y)
+    clf.fit(X_train, y_train)
     coefs.append(clf.coef_)
 
 ax = plt.gca()
