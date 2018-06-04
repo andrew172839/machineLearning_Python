d64a83ca55622959af1914bbc871b86778fec08a
andrew789 <yushao.cheng@gmail.com>
2018-06-04 11:04:51 +0800

    .

---
+++ sklearn/nestedCv.py
@@ -0,0 +1,60 @@
+from matplotlib import pyplot as plt
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV, cross_val_score, KFold
+
+import pandas as pd
+import numpy as np
+from sklearn import datasets
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+
+# a = pd.read_csv('sample20170117_labeled_0207.csv')
+# X = a.values[0: 100, 0: 110]
+# y = a.values[0: 100, 110]
+# y = np.array([1 if i == 1. else -1 for i in y])
+
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+# X, y = make_classification(n_samples=1000, n_features=100, n_classes=2)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+NUM_TRIALS = 30
+p_grid = {"C": [1, 10, 100], "gamma": [.01, .1]}
+svr = SVC(kernel="rbf")
+
+non_nested_scores = np.zeros(NUM_TRIALS)
+nested_scores = np.zeros(NUM_TRIALS)
+
+for i in range(NUM_TRIALS):
+    inner_cv = KFold(n_splits=4, shuffle=True, random_state=i)
+    outer_cv = KFold(n_splits=4, shuffle=True, random_state=i)
+
+    clf = GridSearchCV(estimator=svr, param_grid=p_grid, cv=inner_cv)
+    clf.fit(X_train, y_train)
+    non_nested_scores[i] = clf.best_score_
+
+    nested_score = cross_val_score(clf, X=X_train, y=y_train, cv=outer_cv)
+    nested_scores[i] = nested_score.mean()
+
+score_difference = non_nested_scores - nested_scores
+
+print("average difference of {0:6f} with std dev of {1:6f}.".format(score_difference.mean(), score_difference.std()))
+
+plt.figure()
+plt.subplot(211)
+non_nested_scores_line, = plt.plot(non_nested_scores, color='r')
+nested_line, = plt.plot(nested_scores, color='b')
+plt.ylabel("score", fontsize="14")
+plt.legend([non_nested_scores_line, nested_line], ["non-nested cv", "nested cv"], bbox_to_anchor=(0, .4, .5, 0))
+plt.title("non-nested and nested cross validation", x=0.5, y=1.1, fontsize="15")
+
+plt.subplot(212)
+difference_plot = plt.bar(range(NUM_TRIALS), score_difference)
+plt.xlabel("individual trial #")
+plt.legend([difference_plot], ["non-nested cv - nested cv score"], bbox_to_anchor=(0, 1, .8, 0))
+plt.ylabel("score difference", fontsize="14")
+
+plt.show()
