505cc4c4594338717314e0b1993c20840d9fa15a
andrew789 <yushao.cheng@gmail.com>
2018-06-06 11:26:40 +0800

    .

--- perceptron.py
+++ perceptron.py
@@ -2,10 +2,7 @@ from random import choice
 from numpy import array, dot, random
 
 unit_step = lambda x: 0 if x < 0 else 1
-training_data = [(array([0, 0, 1]), 0),
-                 (array([0, 1, 1]), 1),
-                 (array([1, 0, 1]), 1),
-                 (array([1, 1, 1]), 1), ]
+training_data = [(array([0, 0, 1]), 0), (array([0, 1, 1]), 1), (array([1, 0, 1]), 1), (array([1, 1, 1]), 1), ]
 w = random.rand(3)
 errors = []
 eta = 0.2
@@ -20,4 +17,4 @@ for i in range(n):
 
 for x, _ in training_data:
     result = dot(x, w)
-    print("{}, {} -> {}".format(x[:2], result, unit_step(result)))
+    print('{}, {} -> {}'.format(x[:2], result, unit_step(result)))
