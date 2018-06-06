8e2fa4277f821a78f48769d5f9f118e39889cec7
andrew789 <yushao.cheng@gmail.com>
2018-06-06 11:36:21 +0800

    .

---
+++ sklearn/pipeline.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model, decomposition, datasets
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+
+logistic = linear_model.LogisticRegression()
+
+pca = decomposition.PCA()
+pipe = Pipeline(steps=[('pca', pca), ('logistic', logistic)])
+
+# digits = datasets.load_digits()
+# X = digits.data
+# y = digits.target
+
+import pandas as pd
+import numpy as np
+from sklearn import datasets
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+
+# a = pd.read_csv('sample20170117_labeled_0207.csv')
+# X = a.values[0: 100, 0: 110]
+# y = a.values[0: 100, 110]
+# y = np.array([1 if i == 1. else -1 for i in y])
+
+# iris = datasets.load_iris()
+# X = iris.data
+# y = iris.target
+
+X, y = make_classification(n_samples=1000, n_features=100, n_classes=2)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+pca.fit(X_train)
+
+plt.figure(1, figsize=(4, 3))
+plt.clf()
+plt.axes([.2, .2, .7, .7])
+plt.plot(pca.explained_variance_, linewidth=2)
+plt.axis('tight')
+plt.xlabel('n_components')
+plt.ylabel('explained_variance_')
+
+n_components = [20, 40, 64]
+Cs = np.logspace(-4, 4, 3)
+
+estimator = GridSearchCV(pipe, dict(pca__n_components=n_components, logistic__C=Cs))
+estimator.fit(X_train, y_train)
+
+plt.axvline(estimator.best_estimator_.named_steps['pca'].n_components, linestyle=':', label='n_components chosen')
+plt.legend(prop=dict(size=12))
+plt.show()
