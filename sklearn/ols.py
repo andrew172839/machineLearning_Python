e7fb2ab81da2882d86ceb27734991c623a266d7c
andrew789 <yushao.cheng@gmail.com>
2018-07-09 11:14:34 +0800

    .

--- sklearn/ols.py
+++ sklearn/ols.py
@@ -3,9 +3,11 @@ import numpy as np
 from sklearn import datasets, linear_model
 from sklearn.model_selection import train_test_split
 
+import pandas as pd
+import numpy as np
 from sklearn import datasets
 from sklearn.datasets import make_classification
-import pandas as pd
+from sklearn.model_selection import train_test_split
 
 diabetes = datasets.load_diabetes()
 X = diabetes.data[:, np.newaxis, 2]
@@ -13,6 +15,8 @@ X_train = X[: -20]
 X_test = X[-20:]
 y_train = diabetes.target[: -20]
 y_test = diabetes.target[-20:]
+print('X_train, \n', X_train)
+print('y_train, \n', y_train)
 
 regr = linear_model.LinearRegression()
 regr.fit(X_train, y_train)
