06ab36e4e616cee926c760808d1c8fa1107e6a2b
andrew789 <yushao.cheng@gmail.com>
2018-07-18 10:52:52 +0800

    .

--- tensorflow/linearClassifier.py
+++ tensorflow/linearClassifier.py
@@ -1,19 +1,20 @@
 from sklearn import metrics
 from tensorflow.contrib.learn.python import learn
-import numpy as np
 
+import pandas as pd
+import numpy as np
 from sklearn import datasets
 from sklearn.datasets import make_classification
-import pandas as pd
-
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
+from sklearn.model_selection import train_test_split
 
 # a = pd.read_csv('sample20170117_labeled_0207.csv')
 # X = a.values[0: 100, 0: 110]
 # y = a.values[0: 100, 110]
 # y = np.array([1 if i == 1. else -1 for i in y])
+
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
 
 # X, y = make_classification(n_samples=1000, n_features=100, n_classes=2)
 
