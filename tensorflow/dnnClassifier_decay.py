37b8cc2d12607cd781939f40d3c2e9233a223a2b
andrew789 <yushao.cheng@gmail.com>
2018-07-26 10:14:05 +0800

    .

--- tensorflow/dnnClassifier_decay.py
+++ tensorflow/dnnClassifier_decay.py
@@ -2,14 +2,13 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from sklearn import metrics
-from sklearn.cross_validation import train_test_split
 import tensorflow as tf
 
+import pandas as pd
+import numpy as np
 from sklearn import datasets
 from sklearn.datasets import make_classification
-import pandas as pd
-
-import numpy as np
+from sklearn.model_selection import train_test_split
 
 
 def optimizer_exp_decay():
@@ -20,26 +19,24 @@ def optimizer_exp_decay():
 
 
 def main(unused_argv):
-    # iris = datasets.load_iris()
-    # X = iris.data
-    # y = iris.target
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    a = pd.read_csv('sample20170117_labeled_0207.csv')
+    X = a.values[0: 100, 0: 110]
+    y = a.values[0: 100, 110]
+    y = np.array([1 if i == 1. else -1 for i in y])
 
-    # import pandas as pd
-    # a = pd.read_csv('sample20170117_labeled_0207.csv')
-    # X = a.values[0: 100, 0: 110]
-    # y = a.values[0: 100, 110]
-    # y = np.array([1 if i == 1. else -1 for i in y])
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    iris = datasets.load_iris()
+    X = iris.data
+    y = iris.target
 
     X, y = make_classification(n_samples=1000, n_features=100, n_classes=2)
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
     feature_columns = tf.contrib.learn.infer_real_valued_columns_from_input(X_train)
     classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns, hidden_units=[10, 20, 10], n_classes=2,
                                                 optimizer=optimizer_exp_decay)
 
-    classifier.fit(X_train, y_train, steps=800)
+    classifier.fit(X_train, y_train, steps=500)
     predictions = list(classifier.predict(X_test, as_iterable=True))
     score = metrics.accuracy_score(y_test, predictions)
     print('accuracy, {0:f}'.format(score))
