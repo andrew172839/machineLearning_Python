88ca2f07dbe45f86eba32abf57a3142dc750b972
andrew789 <yushao.cheng@gmail.com>
2018-08-15 09:45:52 +0800

    .

--- sklearn/l1logisticRegressionPath.py
+++ sklearn/l1logisticRegressionPath.py
@@ -1,22 +1,23 @@
 from sklearn import linear_model
 from sklearn.svm import l1_min_c
-import numpy as np
 import matplotlib.pyplot as plt
 
+import pandas as pd
+import numpy as np
 from sklearn import datasets
 from sklearn.datasets import make_classification
-import pandas as pd
+from sklearn.model_selection import train_test_split
+
+a = pd.read_csv('sample20170117_labeled_0207.csv')
+X = a.values[0: 100, 0: 110]
+y = a.values[0: 100, 110]
+y = np.array([1 if i == 1. else -1 for i in y])
 
 # iris = datasets.load_iris()
 # X = iris.data
 # y = iris.target
 
-# a = pd.read_csv('sample20170117_labeled_0207.csv')
-# X = a.values[0: 100, 0: 110]
-# y = a.values[0: 100, 110]
-# y = np.array([1 if i == 1. else -1 for i in y])
-
-X, y = make_classification(n_samples=1000, n_features=100, n_classes=2)
+# X, y = make_classification(n_samples=1000, n_features=100, n_classes=2)
 
 cs = l1_min_c(X, y, loss='log') * np.logspace(0, 3)
 clf = linear_model.LogisticRegression(penalty='l1')
@@ -31,6 +32,5 @@ plt.plot(np.log10(cs), coefs_)
 ymin, ymax = plt.ylim()
 plt.xlabel('log(c)')
 plt.ylabel('coefficients')
-plt.title('logistic regression path')
-plt.axis('tight')
+plt.title('path')
 plt.show()
