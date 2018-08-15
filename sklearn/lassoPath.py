253a5f6db192dd40080498a7ef0e8e6d8fdecd39
andrew789 <yushao.cheng@gmail.com>
2018-08-15 09:51:52 +0800

    .

--- sklearn/lassoPath.py
+++ sklearn/lassoPath.py
@@ -1,26 +1,26 @@
-import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model
-from sklearn.model_selection import train_test_split
 
+import pandas as pd
+import numpy as np
 from sklearn import datasets
 from sklearn.datasets import make_classification
-import pandas as pd
+from sklearn.model_selection import train_test_split
 
 diabetes = datasets.load_diabetes()
 X = diabetes.data
 y = diabetes.target
+
+# a = pd.read_csv('sample20170117_labeled_0207.csv')
+# X = a.values[0: 100, 0: 110]
+# y = a.values[0: 100, 110]
+# y = np.array([1 if i == 1. else -1 for i in y])
 
 # iris = datasets.load_iris()
 # X = iris.data
 # y = iris.target
 
 # X, y = make_classification(n_samples=1000, n_features=100, n_classes=2)
-
-# a = pd.read_csv('sample20170117_labeled_0207.csv')
-# X = a.values[0: 100, 0: 110]
-# y = a.values[0: 100, 110]
-# y = np.array([1 if i == 1. else -1 for i in y])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
@@ -30,10 +30,7 @@ xx = np.sum(np.abs(coefs.T), axis=1)
 xx /= xx[-1]
 
 plt.plot(xx, coefs.T)
-ymin, ymax = plt.ylim()
-plt.vlines(xx, ymin, ymax, linestyle='dashed')
 plt.xlabel('|coef| / max|coef|')
 plt.ylabel('coefficients')
 plt.title('lasso path')
-plt.axis('tight')
 plt.show()
