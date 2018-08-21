aa0c33852435af42bca76816845ee466e8ed39fc
andrew789 <yushao.cheng@gmail.com>
2018-08-21 10:15:35 +0800

    .

--- sklearn/rocCurve.py
+++ sklearn/rocCurve.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import (RandomTreesEmbedding, RandomForestClassifier, GradientBoostingClassifier)
+from sklearn.ensemble import RandomTreesEmbedding, RandomForestClassifier, GradientBoostingClassifier
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import roc_curve
 from sklearn.pipeline import make_pipeline
@@ -10,8 +10,6 @@ import numpy as np
 from sklearn import datasets
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
-
-n_estimator = 10
 
 # a = pd.read_csv('sample20170117_labeled_0207.csv')
 # X = a.values[0: 100, 0: 110]
@@ -27,8 +25,9 @@ X, y = make_classification(n_samples=1000, n_features=100, n_classes=2)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 X_train, X_train_lr, y_train, y_train_lr = train_test_split(X_train, y_train, test_size=0.5)
 
-rt = RandomTreesEmbedding(max_depth=3, n_estimators=n_estimator, random_state=0)
+n_estimator = 10
 
+rt = RandomTreesEmbedding(max_depth=3, n_estimators=n_estimator)
 rt_lm = LogisticRegression()
 pipeline = make_pipeline(rt, rt_lm)
 pipeline.fit(X_train, y_train)
@@ -41,7 +40,6 @@ rf_lm = LogisticRegression()
 rf.fit(X_train, y_train)
 rf_enc.fit(rf.apply(X_train))
 rf_lm.fit(rf_enc.transform(rf.apply(X_train_lr)), y_train_lr)
-
 y_pred_rf_lm = rf_lm.predict_proba(rf_enc.transform(rf.apply(X_test)))[:, 1]
 fpr_rf_lm, tpr_rf_lm, _ = roc_curve(y_test, y_pred_rf_lm)
 
@@ -51,13 +49,10 @@ grd_lm = LogisticRegression()
 grd.fit(X_train, y_train)
 grd_enc.fit(grd.apply(X_train)[:, :, 0])
 grd_lm.fit(grd_enc.transform(grd.apply(X_train_lr)[:, :, 0]), y_train_lr)
-
 y_pred_grd_lm = grd_lm.predict_proba(grd_enc.transform(grd.apply(X_test)[:, :, 0]))[:, 1]
 fpr_grd_lm, tpr_grd_lm, _ = roc_curve(y_test, y_pred_grd_lm)
-
 y_pred_grd = grd.predict_proba(X_test)[:, 1]
 fpr_grd, tpr_grd, _ = roc_curve(y_test, y_pred_grd)
-
 y_pred_rf = rf.predict_proba(X_test)[:, 1]
 fpr_rf, tpr_rf, _ = roc_curve(y_test, y_pred_rf)
 
@@ -85,6 +80,6 @@ plt.plot(fpr_grd, tpr_grd, label='gbt')
 plt.plot(fpr_grd_lm, tpr_grd_lm, label='gbt + lr')
 plt.xlabel('false positive rate')
 plt.ylabel('true positive rate')
-plt.title('roc curve (zoomed in at top left)')
+plt.title('roc curve, zoomed in at top left')
 plt.legend(loc='best')
 plt.show()
